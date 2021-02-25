15593db925857b3d29cf99b735ef0634cfb5ffd8
Putramenio <79514935+Putramenio@users.noreply.github.com>
2021-02-25 13:55:08 +0100

    zadania

---
+++ src/First_week/basick/zadania/zadania.java
@@ -0,0 +1,49 @@
+package First_week.basick.zadania;
+
+public class zadania {
+    public static void main(String[] args) {
+
+        //zad1
+        int myIntA = 22;
+
+        System.out.println("Urodziłem się " +myIntA);
+
+        double myDoubleB = 4.03;
+
+        System.out.println(myDoubleB);
+
+        char myCharC = '%';
+
+        System.out.println("lubie " + myCharC);
+
+        String myStringD ="xDD";
+
+        System.out.println(myStringD);
+
+
+        //zad2
+        int a=5;
+        int b=20;
+
+        double wynik=a*a+b*b;
+
+        System.out.println("wynik wynosi: " +wynik);
+
+
+        //zad3
+        double sqrt = Math.sqrt(3);
+
+        int bok = 7;
+
+        double wynik2 = bok*bok * sqrt/4;
+
+        System.out.println("Pole jest rowne " + wynik2);
+
+
+        //zad4
+        String myStringA = "Antoni";
+        String myStringB = "Kukawka";
+
+        System.out.println(myStringA + " "+ myStringB);
+    }
+}
