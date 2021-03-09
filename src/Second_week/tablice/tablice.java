0d73a6eb7c26b76aff9d3ae688aa4888a7157575
Putramenio <79514935+Putramenio@users.noreply.github.com>
2021-03-09 14:35:29 +0100

    zadania

--- src/Second_week/tablice/tablice.java
+++ src/Second_week/tablice/tablice.java
@@ -1,36 +1,36 @@
 package Second_week.tablice;
 
 public class tablice {
-//    public static void main(String[] args) {
-//        int[][] twoDarray=new int[5][5];
-//        int value=0;
-//        for (int i=0;i<5;i++){
-//            for (int j=0;j<5;j++){
-//                twoDarray[i][j]=value;
-//                value+=5;
-//                System.out.print(twoDarray[i][j]+"\t");
-//            }
-//            System.out.println();
-//        }
-//    }
-//    public static void main(String[] args) {
-//        int[][][][] arr=new int[3][3][3][3];
-//        int value=0;
-//        for (int i=0;i<3;i++){
-//            for (int j=0;j<3;j++){
-//                for (int k=0;k<3;k++){
-//                    for (int l=0;l<3;l++){
-//                        arr[i][j][k][l]=value;
-//                        value+=2;
-//                        System.out.println("["+i+"]"+"["+j+"]"+"["+k+"]"+"["+l+"] = "+arr[i][j][k][l]+ "\t");
-//                    }
-//                    System.out.println();
-//                }
-//                System.out.println();
-//            }
-//            System.out.println();
-//        }
-//    }
+    public static void main(String[] args) {
+        int[][] twoDarray=new int[5][5];
+        int value=0;
+        for (int i=0;i<5;i++){
+            for (int j=0;j<5;j++){
+                twoDarray[i][j]=value;
+                value+=5;
+                System.out.print(twoDarray[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int[][][][] arr=new int[3][3][3][3];
+        int value=0;
+        for (int i=0;i<3;i++){
+            for (int j=0;j<3;j++){
+                for (int k=0;k<3;k++){
+                    for (int l=0;l<3;l++){
+                        arr[i][j][k][l]=value;
+                        value+=2;
+                        System.out.println("["+i+"]"+"["+j+"]"+"["+k+"]"+"["+l+"] = "+arr[i][j][k][l]+ "\t");
+                    }
+                    System.out.println();
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         int[][][][][] arr=new int[2][2][2][2][2];
         int value=0;
